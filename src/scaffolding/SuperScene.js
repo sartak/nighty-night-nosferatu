a028e61e369b97d8c39965f7876ce7d148e4453f
Shawn M Moore <code@sartak.org>
2020-04-15 21:10:07 -0400

    Suppress sounds in preflight mode

--- src/scaffolding/SuperScene.js
+++ src/scaffolding/SuperScene.js
@@ -1697,7 +1697,7 @@ export default class SuperScene extends Phaser.Scene {
 
   playSound(baseName, variants, volume = 1.0) {
     // preflight etc
-    if (!this.scene.isVisible()) {
+    if (!this.scene.isVisible() || game._replayPreflight) {
       return;
     }
 
